22a1ba95d9be04425f1410fd41a499d007b67375
Jay Lim <jlimoman@gmail.com>
2023-10-04 10:27:08 -0600

    create up arrow scroll button on project 1 page, style it, and rename icon files in images foler

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 const logoImg = document.getElementById('#logo-img');
+const toTop = document.querySelectorAll('.to-top-btn');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
