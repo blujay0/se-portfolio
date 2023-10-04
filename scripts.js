a81d28e1999801bf08daa26f07521bdf8d45857f
Jay Lim <jlimoman@gmail.com>
2023-10-04 10:05:18 -0600

    have logo click scroll to top on homepage and navigate to homepage on project pages

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const logoImg = document.getElementById('#logo-img');
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -14,3 +15,12 @@ navLinks.forEach(link => {
 // get date
 let year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
+
+logoImg.addEventListener('click', () => {
+  if (window.location.pathname === '/' || '/#' || 'index.html') {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+})
